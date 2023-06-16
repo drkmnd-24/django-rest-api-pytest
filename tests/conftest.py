166b7b2d407d5edf892a1c676fd32d9bb2cdd8fa
Vanj Mampusti <vanjo.mampusti0324@gmail.com>
2023-06-16 22:11:56 +0800

    create test_endpoint

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,4 +1,7 @@
+import pytest
 from pytest_factoryboy import register
+
+from rest_framework.test import APIClient
 
 from .factories import CategoryFactory, BrandFactory, ProductFactory
 
@@ -6,3 +9,8 @@ from .factories import CategoryFactory, BrandFactory, ProductFactory
 register(CategoryFactory)
 register(BrandFactory)
 register(ProductFactory)
+
+
+@pytest.fixture
+def api_client():
+    return APIClient
