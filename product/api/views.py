77f78e52dca88400843b1d76d674606b13e5fafd
Vanj Mampusti <vanjo.mampusti0324@gmail.com>
2023-06-19 21:03:02 +0800

    add new test endpoints

--- product/api/views.py
+++ product/api/views.py
@@ -30,10 +30,10 @@ class BrandView(viewsets.ViewSet):
 
 class ProductView(viewsets.ViewSet):
     queryset = Product.objects.all().isactive()
-    lookup_field = 'pk'
+    lookup_field = 'slug'
 
-    def retrieve(self, request, pk=None):
-        serializer = ProductSerializer(self.queryset.filter(pk=pk), many=True)
+    def retrieve(self, request, slug=None):
+        serializer = ProductSerializer(self.queryset.filter(slug=slug), many=True)
         return Response(serializer.data, status=status.HTTP_200_OK)
 
     @extend_schema(responses=ProductSerializer)
@@ -41,10 +41,10 @@ class ProductView(viewsets.ViewSet):
         serializer = ProductSerializer(self.queryset, many=True)
         return Response(serializer.data, status=status.HTTP_200_OK)
 
-    @action(methods=['get'], detail=False, url_path=r'category/(?P<category>\w+)/all', url_name='all')
-    def list_product_by_category(self, request, category=None):
+    @action(methods=['get'], detail=False, url_path=r'category/(?P<slug>[\w-]+)')
+    def list_product_by_category_slug(self, request, slug=None):
         """
         endpoint to return products by category
         """
-        serializer = ProductSerializer(self.queryset.filter(category__name=category), many=True)
+        serializer = ProductSerializer(self.queryset.filter(category__slug=slug), many=True)
         return Response(serializer.data, status=status.HTTP_200_OK)
