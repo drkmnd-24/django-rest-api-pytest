6eaa85bc5875da73c66df8e965cc54777864c28f
Vanj Mampusti <vanjo.mampusti0324@gmail.com>
2023-06-19 12:01:26 +0800

    custom name fields in serializers

--- product/api/views.py
+++ product/api/views.py
@@ -3,7 +3,9 @@ from product.models import Category, Brand, Product
 
 from rest_framework import viewsets
 from rest_framework import status
+
 from rest_framework.response import Response
+from rest_framework.decorators import action
 
 from drf_spectacular.utils import extend_schema
 
@@ -28,8 +30,21 @@ class BrandView(viewsets.ViewSet):
 
 class ProductView(viewsets.ViewSet):
     queryset = Product.objects.all()
+    lookup_field = 'pk'
+
+    def retrieve(self, request, pk=None):
+        serializer = ProductSerializer(self.queryset.filter(pk=pk), many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
 
     @extend_schema(responses=ProductSerializer)
     def list(self, request):
         serializer = ProductSerializer(self.queryset, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+    @action(methods=['get'], detail=False, url_path=r'category/(?P<category>\w+)/all', url_name='all')
+    def list_product_by_category(self, request, category=None):
+        """
+        endpoint to return products by category
+        """
+        serializer = ProductSerializer(self.queryset.filter(category__name=category), many=True)
         return Response(serializer.data, status=status.HTTP_200_OK)
