6d5ab4e40488d46c517d15e39f0119f838701d1e
Vanj Mampusti <vanjo.mampusti0324@gmail.com>
2023-06-29 15:29:29 +0800

    refactor admin

--- product/admin.py
+++ product/admin.py
@@ -1,59 +1,62 @@
-# from django.contrib import admin
-# from django.urls import reverse
-# from django.utils.safestring import mark_safe
-#
-# from .models import (
-#     Category, Product, ProductLine,
-#     ProductImage, AttributeValue, Attribute,
-#     ProductType, ProductTypeAttribute, ProductLineAttributeValue)
-#
-#
-# class EditLinkInline(object):
-#     def edit(self, instance):
-#         url = reverse(
-#             f"admin:{instance._meta.app_label}_{instance._meta.model_name}_change",
-#             args=[instance.pk],
-#         )
-#
-#         if instance.pk:
-#             link = mark_safe('<a href="{u}">edit</a>'.format(u=url))
-#             return link
-#         else:
-#             return ''
-#
-#
-# class ProductImageInline(admin.TabularInline):
-#     model = ProductImage
-#
-#
-# class ProductLineInline(EditLinkInline, admin.TabularInline):
-#     model = ProductLine
-#     readonly_fields = ('edit',)
-#
-#
-# class ProductAdmin(admin.ModelAdmin):
-#     inlines = [ProductLineInline]
-#
-#
-# class AttributeValueInline(admin.TabularInline):
-#     model = AttributeValue.product_line_attribute_value.through
-#
-#
-# class ProductLineAdmin(admin.ModelAdmin):
-#     inlines = [ProductImageInline, AttributeValueInline]
-#
-#
-# class AttributeInline(admin.TabularInline):
-#     model = Attribute.product_type_attr.through
-#
-#
-# class ProductTypeAdmin(admin.ModelAdmin):
-#     inlines = [AttributeInline]
-#
-#
-# admin.site.register(Category)
-# admin.site.register(Attribute)
-# admin.site.register(Product, ProductAdmin)
-# admin.site.register(ProductLine, ProductLineAdmin)
-# admin.site.register(AttributeValue)
-# admin.site.register(ProductType, ProductTypeAdmin)
+from django.contrib import admin
+from django.urls import reverse
+from django.utils.safestring import mark_safe
+
+from .models import (
+    Category, Product, ProductLine,
+    ProductImage, AttributeValue, Attribute,
+    ProductType, ProductTypeAttribute, ProductLineAttributeValue)
+
+
+class EditLinkInline(object):
+    def edit(self, instance):
+        url = reverse(
+            args=[instance.pk],
+        )
+
+        if instance.pk:
+            link = mark_safe('<a href="{u}">edit</a>'.format(u=url))
+            return link
+        else:
+            return ''
+
+
+class ProductImageInline(admin.TabularInline):
+    model = ProductImage
+
+
+class ProductLineInline(EditLinkInline, admin.TabularInline):
+    model = ProductLine
+    readonly_fields = ('edit',)
+
+
+class AttributeValueInline(admin.TabularInline):
+    model = AttributeValue.product_line_attribute_value.through
+
+
+class AttributeValueProductInline(admin.TabularInline):
+    model = AttributeValue.product_attr_value.through
+
+
+class ProductAdmin(admin.ModelAdmin):
+    inlines = [ProductLineInline, AttributeValueProductInline]
+
+
+class ProductLineAdmin(admin.ModelAdmin):
+    inlines = [ProductImageInline, AttributeValueInline]
+
+
+class AttributeInline(admin.TabularInline):
+    model = Attribute.product_type_attr.through
+
+
+class ProductTypeAdmin(admin.ModelAdmin):
+    inlines = [AttributeInline]
+
+
+admin.site.register(Category)
+admin.site.register(Attribute)
+admin.site.register(Product, ProductAdmin)
+admin.site.register(ProductLine, ProductLineAdmin)
+admin.site.register(AttributeValue)
+admin.site.register(ProductType, ProductTypeAdmin)
